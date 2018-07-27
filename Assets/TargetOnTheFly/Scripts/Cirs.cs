5af4b8055e5511d00bbd2dbfdfb9bb51040779a6
Daniel0924 <danhaoy94@163.com>
2018-07-27 10:37:45 +0800

    commit

---
+++ Assets/TargetOnTheFly/Scripts/Cirs.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Cirs : MonoBehaviour
+{
+
+    public Material mat;
+
+    //public float bigB = 0.21f;
+    //public float smallB = 0.19f;
+    public float cenx = 0.0f;
+    public float ceny = 0.0f;
+    public float width = 0.3f;
+    public float height = 0.2f;
+    private float thick = 0.002f;
+    public string info = "hello world";
+    
+
+    // Use this for initialization  
+    void Start()
+    {
+        DrawFrame();
+    }
+
+    #region 画方框  
+    void DrawFrame()
+    {
+        //gameObject.AddComponent<MeshFilter>();
+        //gameObject.AddComponent<MeshRenderer>();
+        gameObject.GetComponent<MeshRenderer>().material = mat;
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+
+
+        mesh.vertices = new Vector3[] { new Vector3(-(width/2+thick)+cenx, 0, height/2+thick+ceny), new Vector3(width/2+thick+cenx, 0, height/2+thick+ceny),
+                                        new Vector3(width/2+thick+cenx, 0, -(height/2+thick)+ceny), new Vector3(-(width/2+thick)+cenx, 0, -(height/2+thick)+ceny),
+                                        new Vector3(-width/2+cenx, 0, height/2+ceny), new Vector3(width/2+cenx, 0, height/2+ceny),
+                                        new Vector3(width/2+cenx, 0, -height/2+ceny), new Vector3(-width/2+cenx, 0, -height/2+ceny),
+
+
+        /*
+              mesh.vertices = new Vector3[] { new Vector3(-bigB+cenx, 0, bigB+ceny), new Vector3(bigB+cenx, 0, bigB+ceny),
+                                              new Vector3(bigB+cenx, 0, -bigB+ceny), new Vector3(-bigB+cenx, 0, -bigB+ceny),
+                                              new Vector3(-smallB+cenx, 0, smallB+ceny), new Vector3(smallB+cenx, 0, smallB+ceny),
+                                              new Vector3(smallB+cenx, 0, -smallB+ceny), new Vector3(-smallB+cenx, 0, -smallB+ceny)
+                                              */
+    };
+        mesh.triangles = new int[]
+        { 0, 1, 5,
+          0, 5, 4,
+          1,2,6,
+          1,6,5,
+          2,7,6,
+          2,3,7,
+          3,4,7,
+          0,4,3
+
+        };
+
+
+    }
+    #endregion
+
+
+
+}
---
+++ Assets/TargetOnTheFly/Scripts/TargetOnTheFly.cs
@@ -0,0 +1,144 @@
+//=============================================================================================================================
+//
+// Copyright (c) 2015-2018 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
+// EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
+// and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
+//
+//=============================================================================================================================
+
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using EasyAR;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using CScript;
+using LitJson;
+
+
+namespace Sample
+{
+    public class TargetOnTheFly : MonoBehaviour
+    {
+        private const string title = "Please enter KEY first!";
+        private const string boxtitle = "===PLEASE ENTER YOUR KEY HERE===";
+        private const string keyMessage = ""
+            + "Steps to create the key for this sample:\n"
+            + "  1. login www.easyar.com\n"
+            + "  2. create app with\n"
+            + "      Name: TargetOnTheFly (Unity)\n"
+            + "      Bundle ID: cn.easyar.samples.unity.targetonthefly\n"
+            + "  3. find the created item in the list and show key\n"
+            + "  4. replace all text in TextArea with your key";
+
+        [HideInInspector]
+        public bool StartShowMessage = false;
+        private bool isShowing = false;
+        private bool isClear = false;
+        private ImageTargetManager imageManager;
+        private FilesManager imageCreater;
+
+        public GUISkin skin;
+
+
+        private string message = "Photo Saved";
+        private void Awake()
+        {
+            if (FindObjectOfType<EasyARBehaviour>().Key.Contains(boxtitle))
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorUtility.DisplayDialog(title, keyMessage, "OK");
+#endif
+                Debug.LogError(title + " " + keyMessage);
+            }
+            imageManager = FindObjectOfType<ImageTargetManager>();
+            imageCreater = FindObjectOfType<FilesManager>();
+        }
+        
+        
+
+     
+        void OnGUI()
+        {
+            if (StartShowMessage)
+            {
+                if (!isShowing)
+                    StartCoroutine(showMessage());
+                StartShowMessage = false;
+            }
+
+            //GUI.Box(new Rect(Screen.width / 2 - 250, 30, 500, 60), "The box area will be used as ImageTarget. Take photo!", skin.GetStyle("Box"));
+            //GUI.Box(new Rect(Screen.width / 4, Screen.height / 4, Screen.width / 2, Screen.height / 2), "", skin.GetStyle("Box"));
+
+            //Debug.Log("Input.touchCount:" + Input.touchCount);
+
+            //if (GUI.Button(new Rect(Screen.width - 160, Screen.height - 85, 150, 80), "Clear Targets", skin.GetStyle("Button")))
+            if(GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height - 85, 160, 80), "Clear Targets", skin.GetStyle("Button")))
+            {
+                message = "Photo Clearedddd";
+                imageCreater.ClearTexture();
+                imageManager.ClearAllTarget();
+                isClear = true;
+            }
+
+
+            /**
+            if (GUI.Button(new Rect(10, 70, 100, 40), "start"))
+            {
+                message = "photo catch";
+
+                StartCoroutine(imageCreater.getTexture());
+                //imageCreater.getTexture();
+                
+            }
+            **/
+                
+               
+            if (Input.anyKeyDown && !isClear)
+            {
+                //Debug.Log("Input.touchCount1:" + Input.touchCount);
+                message = "Photo Saved";
+                imageCreater.StartTakePhoto();
+                
+            }
+            isClear = false;
+
+            if (isShowing)
+                GUI.Box(new Rect(Screen.width / 2 - 65, Screen.height / 2, 135, 60), message, skin.GetStyle("Box"));
+
+
+            //if (GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height - 85, 160, 80), "Take Photo", skin.GetStyle("Button")))
+            //imageCreater.StartTakePhoto();
+            //GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height - 85, 160, 80), "Take Photo", skin.GetStyle("Button"));
+
+
+        }
+
+        private WebCamTexture tex;
+        public string deviceName;
+
+        /// <summary>
+        /// 获取截图
+        /// </summary>
+        /// <returns>The texture.</returns>
+        ///
+        ///
+        private string encodePhoto; 
+        
+
+        IEnumerator showMessage()
+        {
+            isShowing = true;
+            yield return new WaitForSeconds(2f);
+            isShowing = false;
+        }
+        
+        
+     
+        
+        
+        
+    }
+}
